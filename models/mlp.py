345fdd23fbb01fd4402decfc02dfcab597478375
hei4 <hei4.1981@gmail.com>
2022-06-25 10:23:35 +0900

    update MLP

--- models/mlp.py
+++ models/mlp.py
@@ -4,10 +4,14 @@ class MLP(nn.Module):
     def __init__(self, num_features=2, num_unit=64, num_classes=2) -> None:
         super().__init__()
         self.layers = nn.Sequential(
-            nn.Linear(num_features, num_unit),
-            nn.ReLU(inplace=True),
-            nn.Linear(num_unit, num_unit),
-            nn.ReLU(inplace=True),
+            nn.Sequential(
+                nn.Linear(num_features, num_unit),
+                nn.ReLU(inplace=True),
+            ),
+            nn.Sequential(
+                nn.Linear(num_unit, num_unit),
+                nn.ReLU(inplace=True),
+            ),
             nn.Linear(num_unit, num_classes)
         )
     
